782055deebfa2c6c5fd08458f2d1507e2dd9017a
Jeff Lapp <lappjeff20@gmail.com>
2019-04-15 18:25:59 -0400

    Added add user actions

---
+++ safe-routes/src/actions/addUser.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export const ADD_USER_START = 'ADD_USER_START'
+export const ADD_USER_SUCCESS = 'ADD_USER_SUCCESS'
+export const ADD_USER_FAIL = 'ADD_USER_FAIL'
+
+export const addUser = info => dispatch => {
+  dispatch ({ type: ADD_USER_START, payload: info })
+
+  return axios
+    .post('https://saferoutes-4-12.herokuapp.com/api/auth/register', info)
+    .then (res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
