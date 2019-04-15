b277988466cdc275b9c2113d2c5767b132e76eef
unknown <lappjeff20@gmail.com>
2019-04-15 10:39:27 -0400

    Set up redux store and imported dependencies in index.js

--- safe-routes/src/index.js
+++ safe-routes/src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+const store = createStore(
+  () => {}, composeWithDevTools (
+    applyMiddleware(thunk)
+  )
+)
+
 ReactDOM.render(<App />, document.getElementById('root'));
