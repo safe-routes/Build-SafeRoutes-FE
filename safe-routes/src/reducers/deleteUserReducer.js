7919105550b54c95cf46496005f2732d7b18a545
Jeff Lapp <lappjeff20@gmail.com>
2019-04-16 13:03:29 -0400

    Set up reducer to delete user

---
+++ safe-routes/src/reducers/deleteUserReducer.js
@@ -0,0 +1,30 @@
+import {
+  DELETE_USER_START,
+  DELETE_USER_SUCCESS,
+  DELETE_USER_FAIL
+} from "../actions";
+
+const initialState = {
+  isDeleting: false
+};
+export const deleteUserReducer = (state = intitialState, action) => {
+  switch (action.type) {
+    case DELETE_USER_START:
+      return {
+        ...state,
+        isDeleting: true
+      };
+    case DELETE_USER_SUCCESS:
+      return {
+        ...state,
+        isDeleting: false
+      };
+    case DELETE_USER_FAIL:
+      return {
+        ...state,
+        isDeleting: false
+      };
+    default:
+      return state;
+  }
+};
