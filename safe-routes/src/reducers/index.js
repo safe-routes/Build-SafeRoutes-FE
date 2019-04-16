49b1509f53120a83fa4f12bc1afa2793a548353b
Jeff Lapp <lappjeff20@gmail.com>
2019-04-16 19:00:18 -0400

    Created update user reducer

--- safe-routes/src/reducers/index.js
+++ safe-routes/src/reducers/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from "redux";
 import { addUserReducer } from "./addUser";
 import { loginReducer } from "./loginReducer";
 import { deleteUserReducer } from "./deleteUserReducer";
-
+import { updateUserReducer } from "./updateUserReducer";
 export default combineReducers({
   addUserReducer,
   loginReducer,
-  deleteUserReducer
+  deleteUserReducer,
+  updateUserReducer
 });
