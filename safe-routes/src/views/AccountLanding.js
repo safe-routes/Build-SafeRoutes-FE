8d3c514bd7dd6f7cef2a3067e1377dab914d7474
Jeff Lapp <lappjeff20@gmail.com>
2019-04-15 20:28:30 -0400

    Created account landing page

---
+++ safe-routes/src/views/AccountLanding.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Authenticate from '../auth/Authenticate'
+
+const AccountLanding = props => {
+  return (
+    <div>Hello from AccountLanding</div>
+  )
+}
+
+export default Authenticate(AccountLanding) 
