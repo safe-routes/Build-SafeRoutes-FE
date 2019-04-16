caed4f8494d01ed91e3a34a502b97aefe5796ff3
Jeff Lapp <lappjeff20@gmail.com>
2019-04-16 12:47:49 -0400

    Added update username input and delete account button to account page

--- safe-routes/src/views/Profile.js
+++ safe-routes/src/views/Profile.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Authenticate from '../auth/Authenticate';
-import { Typography, Row, Col, Input } from 'antd';
+import React from "react";
+import { connect } from "react-redux";
+import Authenticate from "../auth/Authenticate";
+import { Typography, Row, Col, Input, Button } from "antd";
 
 const { Title } = Typography;
 const Profile = props => {
-  const message = localStorage.getItem('greeting');
+  const message = localStorage.getItem("greeting");
 
   return (
     <div>
       <Title level={2}>{message}</Title>
       <Row>
-        <Col xs={{ span: 10, offset: 1 }}>
+        <Col xs={{ span: 20 }}>
           <Input placeholder="Update Username" />
+          <Button block type="danger">
+            Delete Account
+          </Button>
         </Col>
-        <Col xs={{ span: 8, offset: 4 }}>Delete Account</Col>
       </Row>
     </div>
   );
