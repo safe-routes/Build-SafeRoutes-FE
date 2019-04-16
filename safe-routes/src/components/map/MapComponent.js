c595ae648ee7b6b594548b97000a551bddc63af8
Simon Davis <simonfreelance1@gmail.com>
2019-04-16 16:21:50 -0400

    Styled and started implementing search box

--- safe-routes/src/components/map/MapComponent.js
+++ safe-routes/src/components/map/MapComponent.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useContext } from 'react';
+/* eslint-disable no-undef */
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import { compose, withProps } from 'recompose';
 import {
   withScriptjs,
@@ -8,6 +9,9 @@ import {
 } from 'react-google-maps';
 import { markerData } from './data/index';
 import { useMarker } from './UseHooks/index';
+import { SearchBox } from 'react-google-maps/lib/components/places/SearchBox';
+import SearchAddressInput from './PlacesSearchBox/SearchAddressInput';
+
 const MapComponent = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${
@@ -38,17 +42,33 @@ const MapComponent = compose(
     //state
     markers
   } = useMarker();
+  const searchBoxRef = useRef(null);
+  const mapRef = useRef(null);
+  const [bounds, setBounds] = useState(null);
+  const [center, setCenter] = useState({ lat: 36.93, lng: -119.953 });
+  const onBoundsChanged = () => {
+    setBounds(mapRef.getBounds());
+  };
+
   useEffect(() => {
     setInitialMarkers(markerData);
   }, []);
   return (
     <GoogleMap
+      ref={ref => (mapRef.current = ref)}
       defaultZoom={4}
       onClick={e => {
         console.log(e.latLng.lat(), e.latLng.lng());
       }}
       defaultCenter={{ lat: 36.93, lng: -119.953 }}
     >
+      <SearchBox
+        ref={ref => (searchBoxRef.current = ref)}
+        bounds={bounds}
+        controlPosition={google.maps.ControlPosition.TOP_CENTER}
+      >
+        <SearchAddressInput />
+      </SearchBox>
       {markers.map(mark => {
         return <Marker key={mark.id} position={mark.position} />;
       })}
---
+++ safe-routes/src/components/map/PlacesSearchBox/SearchAddressInput.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+const SearchAddressInput = () => {
+  return <Input type="text" placeholder="Enter an address" />;
+};
+
+const Input = styled.input`
+  box-sizing: border-box;
+  border: 1px solid transparent;
+  width: 240px;
+  height: 32px;
+  margin-top: 27px;
+  padding: 0 12px;
+  border-radius: 3px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+  font-size: 14px;
+  outline: none;
+  text-overflow: ellipses;
+`;
+export default SearchAddressInput;
