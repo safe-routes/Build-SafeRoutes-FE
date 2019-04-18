72389d426e51b091895038b2c31df171b258a423
Jeff Lapp <lappjeff20@gmail.com>
2019-04-17 20:22:00 -0400

    Set up create group actions and action creators and started work on reducer. Further work on GroupForm component

--- safe-routes/src/components/GroupForm.js
+++ safe-routes/src/components/GroupForm.js
@@ -1,17 +1,48 @@
 import React, { useState } from 'react';
 import Authenticate from '../auth/Authenticate';
+import { connect } from 'react-redux';
+import { createGroup } from '../actions';
 import { Input, Button } from 'antd';
+
 const GroupForm = props => {
+  const [groupName, setGroupName] = useState('');
+  const [passphrase, setPassphrase] = useState('');
+
+  const useCreateGroup = () => {
+    const groupInfo = {
+      name: groupName,
+      passphrase
+    };
+
+    props.createGroup(groupInfo);
+  };
+
   return (
     <>
-      <h3>Add Group</h3>
+      <h3>Create A New Group</h3>
       <form>
-        <Input placeholder="Group Name" />
-        <Input placeholder="Group Password" />
-        <Button block>Submit Group</Button>
+        <Input
+          value={groupName}
+          onChange={e => setGroupName(e.target.value)}
+          placeholder="Group Name"
+        />
+        <Input
+          value={passphrase}
+          onChange={e => setPassphrase(e.target.value)}
+          placeholder="Group Password"
+          type="password"
+        />
+        <Button onClick={() => useCreateGroup()} block>
+          Create Group
+        </Button>
       </form>
     </>
   );
 };
 
-export default Authenticate(GroupForm);
+export default Authenticate(
+  connect(
+    null,
+    { createGroup }
+  )(GroupForm)
+);
